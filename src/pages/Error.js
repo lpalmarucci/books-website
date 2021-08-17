8783692dc3e267365fa456ce4c73eadb85d2a669
lpalmarucci <lpalmarucci@gmail.com>
2021-08-17 21:15:25 +0200

    inizializzazione error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import GoBackButton from '../components/GoBackButton'
 
 export default function Error() {
     return (
-        <div>
-            Hello from error page
+        <div className="flex flex-container container" style={{ color: 'grey' }}>
+            <GoBackButton destination={'/'} />
+            <br />
+            <br />
+            <h1>:/</h1>
+            <br />
+            <h1>Oops, qualcosa è andato storto!</h1>
+            <br />
+            <h3>La pagina che stai cercando di visitare non è disponibile</h3>
         </div>
     )
 }
