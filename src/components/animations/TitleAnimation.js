8f44e9aa7a3d85f9691c2ef584f180cdb32ce3ea
lpalmarucci <lpalmarucci@gmail.com>
2022-01-24 15:18:19 +0100

    Added animation title

---
+++ src/components/animations/TitleAnimation.js
@@ -0,0 +1,39 @@
+import React from "react"
+import styled, { keyframes } from "styled-components"
+
+export default function TitleAnimation() {
+  return <Title>Books Website</Title>
+}
+
+const animation = keyframes`
+    
+    0%{
+        transform: translateY(-10px);
+        letter-spacing: 0em;
+        filter: hue-rotate(0deg);
+    }
+
+    100%{
+        transform: translateY(10px);
+        letter-spacing: .02em;
+        filter: hue-rotate(20deg);
+    }
+`
+
+const Title = styled.h1`
+  font-size: 70px;
+  color: white;
+  padding: 10px;
+  background: linear-gradient(120deg, #1c045d 0%, #412a83 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  font-weight: 800;
+  outline: none;
+
+  animation: ${animation} 3s ease-in alternate infinite;
+
+  @media (max-width: 880px) {
+    font-size: 50px;
+  }
+`
