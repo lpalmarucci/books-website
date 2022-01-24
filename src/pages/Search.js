93ec7e8b1eb613377e4eece526266d1a4c40a766
lpalmarucci <lpalmarucci@gmail.com>
2022-01-24 15:24:54 +0100

    removed global styles for text

---
+++ src/pages/Search.js
@@ -0,0 +1,28 @@
+import React from "react"
+import SearchBox from "../components/SearchBox"
+import BooksContainer from "../components/BooksContainer"
+import styled from "styled-components"
+import TitleAnimation from "../components/animations/TitleAnimation"
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <TitleWrapper>
+        <TitleAnimation>Books Website</TitleAnimation>
+      </TitleWrapper>
+      <SearchBox />
+      <BooksContainer />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 80px 20px 10px 20px;
+  display: grid;
+  grid-template-rows: 150px 144px auto;
+  gap: 40px;
+`
+const TitleWrapper = styled.div`
+  text-align: center;
+`
