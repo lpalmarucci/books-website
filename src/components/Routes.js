b3c3f3be381ab4a340b7a3afcc78d327f3c5b36e
lpalmarucci <lpalmarucci@gmail.com>
2022-01-24 14:38:47 +0100

    add /search route

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom';
-import Book from '../pages/Book';
-import Error from '../pages/Error';
-import Home from '../pages/Home';
-import SavedBooks from '../pages/SavedBooks';
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import Book from "../pages/Book"
+import Error from "../pages/Error"
+import Home from "../pages/Home"
+import Search from "../pages/Search"
+import SavedBooks from "../pages/SavedBooks"
 
 export default function Routes() {
-    return (
-        <>
-            <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="/book/:id" exact>
-                    <Book drawGoBack={true} />
-                </Route>
-                <Route path="/books/saved" component={SavedBooks} exact />
-                <Route component={Error} />
-            </Switch>
-        </>
-    )
+  return (
+    <>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/search" component={Search} exact />
+        <Route path="/book/:id" exact>
+          <Book drawGoBack={true} />
+        </Route>
+        <Route path="/books/saved" component={SavedBooks} exact />
+        <Route component={Error} />
+      </Switch>
+    </>
+  )
 }
