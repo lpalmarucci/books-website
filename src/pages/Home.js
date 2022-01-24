e395ee562be4ee34a1fd4a8ce8903493ce3f4583
lpalmarucci <lpalmarucci@gmail.com>
2022-01-24 15:25:06 +0100

    added title animation Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,14 @@ import React from "react"
 import SearchBox from "../components/SearchBox"
 import BooksContainer from "../components/BooksContainer"
 import styled from "styled-components"
+import TitleAnimation from "../components/animations/TitleAnimation"
 
 export default function Home() {
   return (
     <Wrapper>
-      <Title>Books Website</Title>
-      <SearchBox />
-      <BooksContainer />
+      <TitleWrapper>
+        <TitleAnimation>Books Website</TitleAnimation>
+      </TitleWrapper>
     </Wrapper>
   )
 }
@@ -17,12 +18,9 @@ const Wrapper = styled.div`
   width: 100%;
   padding: 80px 20px 10px 20px;
   display: grid;
-  grid-template-rows: 50px 144px auto;
+  grid-template-rows: 150px 144px auto;
   gap: 40px;
 `
-
-const Title = styled.h1`
-  font-size: 60px;
-  color: white;
+const TitleWrapper = styled.div`
   text-align: center;
 `
